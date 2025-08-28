7de5fa13bc3c2c58c598ccc65a2b55c2534781a8
Andrey Kim <nptm.group@gmail.com>
2025-08-28 16:29:06 +0300

    kim: fix all

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,4 @@
-export const prerender = false;
-export const trailingSlash = 'always';
+export const ssr = false; // рендерим только на клиенте
+export const csr = true; // включаем клиентский рендер
+export const prerender = false; // отключаем полный пререндер, иначе будут ошибки из-за динамических роутов
+// export const trailingSlash = 'always'; // по желанию: можно включить, если нужен финальный слэш

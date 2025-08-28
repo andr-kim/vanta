2cc483fc0ca55600e0094f73658b9e63e13baae8
Andrey Kim <nptm.group@gmail.com>
2025-08-28 16:05:02 +0300

    kim: add types/node

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,20 @@
-export const prerender = true;
-export const trailingSlash = 'always';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
+
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/vanta'
+		},
+		prerender: {
+			entries: ['/vanta'] // ⚡ явно указываем базовый маршрут
+		}
+	}
+};
+
+export default config;
